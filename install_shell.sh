a01b8b70ea8d8557d752482e2d492e7d2d0029d0
DavidMarzocca <david.marzocca@gmail.com>
2015-10-26 22:13:25 +0100

    Create install_shell.sh

---
+++ install_shell.sh
@@ -0,0 +1,14 @@
+#
+# This script creates a cron job which runs the python script every 30 minutes.
+#
+#
+# You can see how many jobs are currently scheduled from inside of cron by typing the following command:
+# crontab -l
+#
+
+ThisFolder="$(pwd)"
+
+python DSCOVR.py
+
+(crontab -l 2>/dev/null; echo " */30 * * * * cd "$ThisFolder"/; python DSCOVR.py >/dev/null 2>&1") | crontab -
+
