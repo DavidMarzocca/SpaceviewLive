8dc7e29dc3244858ad7ff6f21b51b033994d9030
DavidMarzocca <david.marzocca@gmail.com>
2015-10-29 12:02:35 +0100

    Update DSCOVR.py

--- DSCOVR.py
+++ DSCOVR.py
@@ -45,48 +45,60 @@ def download_photo(img_url, filename):
         return False
     return True
 
-
-# I want it to download two-days-ago photo from the same hour we are in
-# The reason for the two days is because the photos are uploaded
-# with 24-36 hours of delay from the time they are taken
 today = datetime.datetime.utcnow()
-two_day = datetime.timedelta(days=2)
-yesterday = today - two_day
 
-datetag = str(yesterday.year) + str(yesterday.month).zfill(2) + str(yesterday.day).zfill(2)
+# Starting from today, it will go back in time day by day (up to 1 month)
+# until it finds a photo with the time within 2 hours of the present time.
+# This allows to avoid some breaks which happen when the satellite does not upload new photos.
+for deltaDay in range(0, 30):
 
+	delta_day = datetime.timedelta(days=deltaDay)
+	photo_day = today - delta_day
 
-# I open the archive webpage and download it in a buffer
-# This archive contains metadata for all photos taken in a given day
+	datetag = str(photo_day.year) + str(photo_day.month).zfill(2) + str(photo_day.day).zfill(2)
 
-urlarchive = 'http://epic.gsfc.nasa.gov/api/images.php?date='
+	# I open the archive webpage and download it in a buffer
+	# This archive contains metadata for all photos taken in a given day
 
-archive_response = urllib.urlopen(urlarchive + datetag)
+	urlarchive = 'http://epic.gsfc.nasa.gov/api/images.php?date='
 
-buf_archive = archive_response.read()
+	archive_response = urllib.urlopen(urlarchive + datetag)
 
-# Now with a regular expression I extract the positions of the photos filenames
-list_index_filename = [m.start() for m in re.finditer('epic_1b_', buf_archive)]
+	buf_archive = archive_response.read()
 
-# From these positions I get the datecode of each photo: yyyymmddhhmmss
-photo_datecode = []
-for ii in list_index_filename:
-	photo_datecode.append(buf_archive[ii + 8: ii + 22])
-
-# I close the webpage
-archive_response.close()
-
-# this is the present timestamp: time in seconds from a given date
-timestamp_yest = (yesterday - datetime.datetime(1970, 1, 1)).total_seconds()
-
-# I create a list of timestamps from the date codes in the web archive
-rel_photo_timestamp = []
-for datecode in photo_datecode:
-	date_time = datetime.datetime(int(datecode[0:4]), int(datecode[4:6]), int(datecode[6:8]), int(datecode[8:10]), int(datecode[10:12]), int(datecode[12:14]))
-	rel_photo_timestamp.append( abs((date_time - datetime.datetime(1970, 1, 1)).total_seconds() - timestamp_yest ))
+	# Now with a regular expression I extract the positions of the photos filenames
+	list_index_filename = [m.start() for m in re.finditer('epic_1b_', buf_archive)]
 	
-# I find the instance in the list closer in time with the present time
-min_deltat_index = rel_photo_timestamp.index(min(rel_photo_timestamp))
+	# If in that day there are no photos it skips to the previous day
+	if len(list_index_filename) == 0:
+		continue
+	
+	# From these positions I get the datecode of each photo: yyyymmddhhmmss
+	photo_datecode = []
+	for ii in list_index_filename:
+		photo_datecode.append(buf_archive[ii + 8: ii + 22])
+
+	# I close the webpage
+	archive_response.close()
+
+	# this is the timestamp of the time I'm looking the photo for: time in seconds from a given date
+	timestamp_photo_day = (photo_day - datetime.datetime(1970, 1, 1)).total_seconds()
+
+	# I create a list of timestamps from the date codes in the web archive
+	rel_photo_timestamp = []
+	for datecode in photo_datecode:
+		date_time = datetime.datetime(int(datecode[0:4]), int(datecode[4:6]), int(datecode[6:8]), int(datecode[8:10]), int(datecode[10:12]), int(datecode[12:14]))
+		rel_photo_timestamp.append( abs((date_time - datetime.datetime(1970, 1, 1)).total_seconds() - timestamp_photo_day ))
+	
+	# I find the instance in the list closer in time with the present time
+	min_deltat_index = rel_photo_timestamp.index(min(rel_photo_timestamp))
+	
+	# I want the photo to be within 2 hours of the present time, otherwise I search for a previous day
+	if min(rel_photo_timestamp) < 2 * 3600:
+		break
+
+
+# End of the For loop.
 
 # This is the datecode of the photo I will download
 datecode = photo_datecode[min_deltat_index]
@@ -104,4 +116,3 @@ shutil.copy2(DOWNLOADED_IMAGE_PATH + downloaded_photoname, DOWNLOADED_IMAGE_PATH
 
 # The End
 raise SystemExit()
-
